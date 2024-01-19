af3f0e0bf5b31c3c5ef022d3ffaec246059a790d
Chandan Kumar <115595327+ckyt@users.noreply.github.com>
2024-01-19 17:49:54 +0530

    Update routes.jsx
    Delete the comment lines

--- src/Routes/routes.jsx
+++ src/Routes/routes.jsx
@@ -1,7 +1,7 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import PokeDetails from '../Component/Complete Details/Cdetails';
 import List from '../Component/Pokemon List/List';
-// import Same from '../Component/Recommend/Same';
+
 function  CustomRoutes (){
    return (
     <>
@@ -9,7 +9,7 @@ function  CustomRoutes (){
     <Routes>
         <Route path='/pokedetails/:id' element={<PokeDetails/>}/>
         <Route path='/' element={<List/>}/> 
-        {/* <Route path='/hii' element = {<Same/>}/> */}
+        
     </Routes>
     </Router>
     
